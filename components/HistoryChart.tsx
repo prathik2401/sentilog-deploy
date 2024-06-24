e9989d9303b47c68bf2930b13aa69022b4a895af
Sai Prathik R <saiprathik2419@gmail.com>
2024-06-24 11:48:18 +0530

    fix: history chart padding

--- components/HistoryChart.tsx
+++ components/HistoryChart.tsx
@@ -1,20 +1,20 @@
-'use client'
-import { ResponsiveContainer, LineChart, Line, XAxis, Tooltip } from 'recharts'
+"use client";
+import { ResponsiveContainer, LineChart, Line, XAxis, Tooltip } from "recharts";
 
 const CustomTooltip = ({ payload, label, active }) => {
-  const dateLabel = new Date(label).toLocaleString('en-us', {
-    weekday: 'long',
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-  })
+  const dateLabel = new Date(label).toLocaleString("en-us", {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
 
   if (active) {
-    const analysis = payload[0].payload
+    const analysis = payload[0].payload;
     return (
-      <div className=" custom-tooltip bg-white/5 shadow-md border border-zinc-700 rounded-lg backdrop-blur-md relative">
+      <div className="p-8 custom-tooltip bg-white/5  shadow-md border border-zinc-700 rounded-lg backdrop-blur-md relative">
         <div
           className="absolute left-2 top-2 w-2 h-2 rounded-full"
           style={{ background: analysis.color }}
@@ -24,11 +24,11 @@ const CustomTooltip = ({ payload, label, active }) => {
         </p>
         <p className="intro text-xl uppercase font-semibold">{analysis.mood}</p>
       </div>
-    )
+    );
   }
 
-  return null
-}
+  return null;
+};
 
 const HistoryChart = ({ data }) => {
   return (
@@ -45,7 +45,7 @@ const HistoryChart = ({ data }) => {
         <Tooltip content={<CustomTooltip />} />
       </LineChart>
     </ResponsiveContainer>
-  )
-}
+  );
+};
 
-export default HistoryChart
+export default HistoryChart;
